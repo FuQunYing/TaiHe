adf0fd860cade5979abf796564dc2dd6811f7eaf
FuQunYing <33327558+FuQunYing@users.noreply.github.com>
2017-11-21 01:02:16 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -5,14 +5,17 @@ $.get("header.html")
     $("header").html(html)
   })
   .then(()=>{
-  "use strict";
-    $(".mynav ul li a").on("click",function(){
-      if($(this).next().css("display")=="none"){
-        $(this).next().css("display","block")
-      }else{
-        $(this).next().css("display","none")
-      }
+    $(".mynav ul>li>a").on("click",function(){
+        $(this).parent().addClass("showList");
+        $(this).parent().siblings().removeClass("showList");
+        $(this).parent().parent().next().children("li").removeClass("showList");
+        $(this).parent().parent().prev().children("li").removeClass("showList");
+      console.log(1);
     });
+    $(".mynav ul>li>a").next().on("mouseleave",function(){
+      $(this).parent().removeClass("showList");
+    });
+  
     $(".wechat").on("mouseenter",function(){
       $(this).next().show()
     }).on("mouseleave",function () {
